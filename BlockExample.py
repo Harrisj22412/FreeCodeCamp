35a6b1f85158a9f80959cbb19f0a456596dae815
Jalen Harris <Harrisj22412@hocking.edu>
2021-03-22 06:45:01 -0400

    Add files via upload

---
+++ BlockExample.py
@@ -0,0 +1,8 @@
+def welcome(year):
+    if year == 'frosh':
+        print('Welcome to BU!')
+        print('Have a great four years!')
+    else:
+        print('Welcome back')
+        print('Have a great semster!')
+        print('Be nice to the frosh students.')
